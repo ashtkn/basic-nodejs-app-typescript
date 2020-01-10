8a00c7a6b6c135e763946e61e80fc0476fdcdd8e
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-01-10 12:26:52 +0900

    Setup basic files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  plugins: [
+    '@typescript-eslint'
+	],
+  extends: [
+    'eslint:recommended',
+    "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      ecmaVersion: 2019,
+      sourceType: 'module',
+  },
+  rules: {}
+}
---
+++ src/hi.ts
@@ -0,0 +1,5 @@
+function hi(name: string): string {
+  return `Hi! ${name}`
+}
+
+export default hi
